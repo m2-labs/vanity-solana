31e3f4c00471b5b692b88b7ff21de7b4a129f109
Matt Venables <matt@venabl.es>
2021-10-29 20:02:24 -0400

    Add suffix and verbose options

--- lib/index.ts
+++ lib/index.ts
@@ -1,29 +1,63 @@
 import { Keypair } from "@solana/web3.js"
+import { Command } from "commander"
 
-async function generateVanityWallet(startsWith: string): Promise<void> {
-  let keypair = await Keypair.generate()
-  console.log(keypair.publicKey.toBase58())
+type Opts = { verbose?: boolean; prefix?: string; suffix?: string }
 
-  while (
-    !keypair.publicKey
-      .toBase58()
-      .toLowerCase()
-      .startsWith(startsWith.toLowerCase())
-  ) {
-    keypair = await Keypair.generate()
-    console.log(keypair.publicKey.toBase58())
+function matches(address: string, { prefix, suffix }: Opts): boolean {
+  let prefixMatch = true
+  let suffixMatch = true
+
+  if (prefix) {
+    prefixMatch = address.toLowerCase().startsWith(prefix.toLowerCase())
   }
 
-  console.log("\n\n")
-  console.log("Public Key: ", keypair.publicKey.toBase58())
-  console.log("Private Key: ", Buffer.from(keypair.secretKey).toString("hex"))
+  if (suffix) {
+    suffixMatch = address.toLowerCase().endsWith(suffix.toLowerCase())
+  }
+
+  return prefixMatch && suffixMatch
 }
 
-const prefix = process.argv[2]
-if (!prefix || prefix.length < 1) {
-  console.log("Please provide a prefix")
+async function generateVanityWallet({
+  verbose,
+  prefix,
+  suffix
+}: Opts): Promise<void> {
+  const opts = []
+  if (prefix && prefix.length) opts.push(`prefix "${prefix}"`)
+  if (suffix && suffix.length) opts.push(`suffix "${suffix}"`)
+  console.log(`Generating vanity wallet with ${opts.join(" and ")} ...`)
+
+  let keypair: Keypair
+
+  do {
+    keypair = Keypair.generate()
+    if (verbose) {
+      console.log(`${keypair.publicKey.toBase58()}`)
+    }
+  } while (!matches(keypair.publicKey.toBase58(), { prefix, suffix }))
+
+  console.log("\n\n----")
+  console.log("Public Key:")
+  console.log(keypair.publicKey.toBase58())
+  console.log("\nPrivate Key:")
+  console.log(Buffer.from(keypair.secretKey).toString("hex"))
+  console.log("----")
+}
+
+const program = new Command()
+program
+  .option("-v, --verbose", "output all addressees as they are generated")
+  .option("-p, --prefix <prefix>", "prefix of the address")
+  .option("-s, --suffix <suffix>", "suffix of the address")
+
+program.parse(process.argv)
+
+const { verbose, prefix, suffix } = program.opts()
+
+if ((!prefix || !prefix.length) && (!suffix || !suffix.length)) {
+  console.error("Please specify a --prefix or --suffix")
   process.exit(1)
 }
 
-console.log(`Generating vanity wallet with prefix "${prefix}"`)
-generateVanityWallet(prefix)
+generateVanityWallet({ verbose, prefix, suffix })
